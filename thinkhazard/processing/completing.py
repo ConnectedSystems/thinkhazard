0229a717e4e496b231eed63a462d8e7405dd023d
ConnectedSystems <takuyai@gmail.com>
2019-11-11 01:04:30 +1100

    Replaced deprecated context
    rasterio env

--- thinkhazard/processing/completing.py
+++ thinkhazard/processing/completing.py
@@ -20,6 +20,8 @@
 import logging
 import transaction
 import rasterio
+import rasterio.env
+
 from sqlalchemy import func
 
 from ..models import (
@@ -123,7 +125,7 @@ class Completer(BaseProcessor):
             if not layer.downloaded:
                 return 'No data for layer {}'.format(layer.name())
             try:
-                with rasterio.drivers():
+                with rasterio.Env():
                     with rasterio.open(self.layer_path(layer)) as reader:
                         bounds = reader.bounds
                         if bounds.bottom > bounds.top:
